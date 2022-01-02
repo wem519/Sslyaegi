26cf49e701e33dd21bbb849a3447d3f0492bf1dd
wem519 <wem519@gmail.com>
2022-01-03 08:15:49 +0900

    boardlist

--- src/components/ssnyaegi/Ssnyaegi.presenter.tsx
+++ src/components/ssnyaegi/Ssnyaegi.presenter.tsx
@@ -4,7 +4,14 @@ export default function SsnyaegiUI(props) {
   return (
     <>
       <Title>SSNYAEGI</Title>
-
+      {props.data?.fetchBoards.map((el, index) => (
+        <div key={el._id}>
+          <div>{el.title}</div>
+          <div>{el.writer}</div>
+          <div>{el.createdAt}</div>
+          <div></div>
+        </div>
+      ))}
       <div></div>
     </>
   );
