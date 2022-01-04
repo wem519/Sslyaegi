f914b05ed9e9181488f2dea6d127f0ba8c7f290b
wem519 <wem519@gmail.com>
2022-01-04 20:42:34 +0900

    list css

--- src/components/ssnyaegi/Ssnyaegi.presenter.tsx
+++ src/components/ssnyaegi/Ssnyaegi.presenter.tsx
@@ -1,18 +1,36 @@
-import { Title } from "./Ssnyaegi.styles";
+import {
+  Wrapper,
+  Title,
+  Board,
+  Row,
+  ColumnNumber,
+  ColumnTitle,
+  ColumnWriter,
+  ColumnDate,
+} from "./Ssnyaegi.styles";
 
 export default function SsnyaegiUI(props) {
   return (
-    <>
+    <Wrapper>
       <Title>SSNYAEGI</Title>
-      {props.data?.fetchBoards.map((el, index) => (
-        <div key={el._id}>
-          <div>{el.title}</div>
-          <div>{el.writer}</div>
-          <div>{el.createdAt}</div>
-          <div></div>
-        </div>
-      ))}
+      <Board>
+        <Row>
+          <ColumnNumber>번호</ColumnNumber>
+          <ColumnTitle>제목</ColumnTitle>
+          <ColumnWriter>작성자</ColumnWriter>
+          <ColumnDate>날짜</ColumnDate>
+        </Row>
+        {props.data?.fetchBoards.map((el, index) => (
+          <Row key={el._id}>
+            <ColumnNumber>{index + 1}</ColumnNumber>
+            <ColumnTitle>{el.title}</ColumnTitle>
+            <ColumnWriter>{el.writer}</ColumnWriter>
+            <ColumnDate>{el.createdAt.slice(0, 10)}</ColumnDate>
+            <div></div>
+          </Row>
+        ))}
+      </Board>
       <div></div>
-    </>
+    </Wrapper>
   );
 }
--- src/components/ssnyaegi/Ssnyaegi.styles.ts
+++ src/components/ssnyaegi/Ssnyaegi.styles.ts
@@ -1,6 +1,39 @@
 import styled from "@emotion/styled";
 
-export const Title = styled.div`
+export const Wrapper = styled.div`
   font-family: "myfont";
-  font-size: 20px;
+  font-size: 15px;
+  text-align: center;
+`;
+export const Title = styled.div`
+  margin-top: 20px;
+  font-size: 40px;
+`;
+export const Board = styled.div`
+  margin-top: 40px;
+`;
+export const Row = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  height: 52px;
+  line-height: 52px;
+  border-bottom: 1px solid black;
+`;
+
+export const ColumnNumber = styled.div`
+  width: 10%;
+  text-align: center;
+`;
+export const ColumnWriter = styled.div`
+  width: 10%;
+  text-align: center;
+`;
+export const ColumnTitle = styled.div`
+  width: 60%;
+  text-align: center;
+`;
+export const ColumnDate = styled.div`
+  width: 20%;
+  text-align: center;
 `;
